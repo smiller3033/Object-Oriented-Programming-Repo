3ae769500113c94aceb4149184cab22e3e81fbd8
Stephen Miller <smiller2000@comcast.net>
2022-10-12 15:15:10 -0400

    Calculator

--- Course 1/HW1 - Intro to Variables and Operators/Bad3.java
+++
@@ -1,7 +0,0 @@
-public class Bad3 {
-    public static void main(String[] args) {
-        String letter = "a";
-        System.out.println("letter is " + letter);
-    }
-}
-
--- Course 2/HW5 - Amidst Us/Impostor.java
+++
@@ -1,6 +0,0 @@
-public interface Impostor {
-
-    public void freeze(Player p);
-
-    public void sabotage(Player p);
-}
